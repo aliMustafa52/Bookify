818740efa1bb99768d6632df6a040dadde1b31e3
Ali Mustafa Ali Mohamed <alimustafa6267@gmail.com>
2023-10-02 14:58:10 +0300

    Add AuthorsController

---
+++ Bookify.web/Controllers/AuthorsController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.web.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var authors = _context.Authors.AsNoTracking().ToList();
+            var viewModel = _mapper.Map<IEnumerable<CategoryViewModel>>(authors);
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        [AjaxOnly]
+        public IActionResult Create()
+        {
+            return PartialView("_Form");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AuthorFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var author = _mapper.Map<Author>(model);
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+
+
+            var viewModel = _mapper.Map<AuthorViewModel>(author);
+            return PartialView("_AuthorRow", viewModel);
+
+        }
+
+        [HttpGet]
+        [AjaxOnly]
+
+        public IActionResult Edit(int id)
+        {
+            var author = _context.Authors.Find(id);
+            if (author is null)
+                return NotFound();
+
+            var viewModel = _mapper.Map<AuthorFormViewModel>(author);
+            return PartialView("_Form", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(AuthorFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var author = _context.Authors.Find(model.Id);
+            if (author is null)
+                return NotFound();
+
+            author = _mapper.Map(model, author);
+            author.LastUpdatedOn = DateTime.Now;
+
+            _context.Authors.Update(author);
+            _context.SaveChanges();
+
+            var viewModel = _mapper.Map<AuthorViewModel>(author);
+            return PartialView("_AuthorRow", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ToggleStatus(int id)
+        {
+            var author = _context.Authors.Find(id);
+            if (author is null)
+                return NotFound();
+
+            author.IsDeleted = !author.IsDeleted; // best way to toggle boolean
+            // long way to toggle boolean
+            //if(category.IsDeleted)
+            //    category.IsDeleted = false;
+            //else
+            //    category.IsDeleted = true;
+
+            author.LastUpdatedOn = DateTime.Now;
+
+            _context.SaveChanges();
+            return Ok(author.LastUpdatedOn.ToString());
+        }
+
+        public IActionResult AllowItem(AuthorFormViewModel model)
+        {
+            var author = _context.Authors.SingleOrDefault(c => c.Name == model.Name);
+            var isAllowed = author is null || author.Id.Equals(model.Id);
+            return Json(isAllowed);
+        }
+
+    }
+}
